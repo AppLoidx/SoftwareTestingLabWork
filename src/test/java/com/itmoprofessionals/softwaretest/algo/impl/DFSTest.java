7e3df3c1f9a0ee2270196a2ab7a56d621649923e
apploid <apploidyakutsk@gmail.com>
2021-02-21 23:46:23 +0300

    fix test

--- src/test/java/com/itmoprofessionals/softwaretest/algo/impl/DFSTest.java
+++ src/test/java/com/itmoprofessionals/softwaretest/algo/impl/DFSTest.java
@@ -4,7 +4,6 @@ import com.itmoprofessionals.softwaretest.graph.model.Node;
 import com.itmoprofessionals.softwaretest.util.GraphUtil;
 import org.junit.jupiter.api.Test;
 
-import java.util.LinkedHashSet;
 import java.util.LinkedList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,6 +11,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class DFSTest {
+
+
     @Test
     void dfs_sampleCheck() {
         Node sampleNode = GraphUtil.createSampleGraph();
@@ -21,10 +22,20 @@ class DFSTest {
         assertEquals(1, path.get(0).getId());
         assertEquals(2, path.get(1).getId());
         Node node3 = path.get(2);
+
+        /*
+
+        из-за того что соседние ноды хранятся в неупорядоченном виде
+        мы должны рассмотреть все варианты когда алгоритм выбирает одну или другую
+        соседнюю ноду
+
+        в этом случае, алгоритм может пройти через ноду 4 или 3 в зависимости от реализации
+
+        */
         assertTrue(node3.getId() == 4 || node3.getId() == 3);
         assertEquals(5, path.get(3).getId());
         Node node5 = path.get(4);
-        assertTrue(node3.getId() == 4 || node3.getId() == 3);
+        assertTrue(node5.getId() == 4 || node5.getId() == 3);
     }
 
 }
