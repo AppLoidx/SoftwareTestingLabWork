263b1121d43904173ea37b481cc942f92038963b
Arthur <apploidyakutsk@gmail.com>
2021-03-04 13:04:54 +0300

    add DFS algorithm

--- src/main/java/com/itmoprofessionals/softwaretest/graph/algo/DFS.java
+++ src/main/java/com/itmoprofessionals/softwaretest/graph/algo/DFS.java
@@ -1,12 +1,15 @@
 package com.itmoprofessionals.softwaretest.graph.algo;
 
 import com.itmoprofessionals.softwaretest.graph.model.Node;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Stack;
 
 public final class DFS {
+    private static final Logger log = LoggerFactory.getLogger(DFS.class);
 
     private DFS() {}
 
@@ -17,6 +20,7 @@ public final class DFS {
      * @param nodeObserver observer that will be applied to all visited nodes
      */
     public static void applyTo(final Node startNode, final NodeObserver nodeObserver) {
+        log.debug("Start DFS algorithm");
 
         final Set<Node> visited = new HashSet<>();
         final Stack<Node> stack = new Stack<>();
@@ -27,6 +31,7 @@ public final class DFS {
             Node current = stack.pop();
             visited.add(current);
             nodeObserver.onNode(current);
+            log.debug("Visit node number : " + current.getId());
             for (Node node : current.getTargets()) {
                 if (!visited.contains(node)) {
                     stack.push(node);
